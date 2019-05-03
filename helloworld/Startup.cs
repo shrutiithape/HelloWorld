e572bfe6443d5c4a84962de452cf9d1501267ea5
Ithape <shruti.ithape@capgemini.com>
2019-05-03 14:48:34 +0530

    My Project

---
+++ helloworld/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(helloworld.Startup))]
+namespace helloworld
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
